a2a90b55a4d5e673d9390db8f74eccee6e02ff9d
edwin perez <abayarde21@gmail.com>
2023-04-26 23:34:54 -0600

    merge pedidos model

---
+++ application/models/pedidos_model.php
@@ -0,0 +1,67 @@
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class pedidos_model extends CI_Model
+{
+
+	public $tabla;
+    public function __construct()
+    {
+        $this->tabla = 'ma_pedidos';
+        parent::__construct();
+    }
+
+
+	public function agregar($dataContent = array())
+    {
+       
+        $insert_id = ($this->db->insert($this->tabla, $dataContent) == true) ? $this->db->insert_id() : false;
+       
+        return $insert_id;
+    }
+
+	public function eliminar($id = 0){
+
+		if ($id != 0) {
+			$CadenaSQL = "DELETE FROM ".$this->tabla." WHERE id=".$id;
+
+			if($this->db->query($CadenaSQL)){
+				return TRUE;
+			}else{
+				return FALSE;
+			}
+		}
+	}
+
+	public function getAll($condicion = null){
+
+		
+		if ($condicion != null) {
+			$this->db->where($condicion);
+			# code...
+		}else{
+			$this->db->where('1=1');
+		}
+
+		$result= $this->db
+		->select('de_menu.*,ma_menus.fecha_menu')
+		->from($this->tabla)
+        ->join('ma_menus', 'de_menu.id_ma_menu = ma_menus.id')
+		->get();
+
+		return $result->result();
+
+	}
+
+	public function detalle($id_ma_menu= 0){
+		
+		$result= $this->db
+		->select('*')
+		->from($this->tabla)
+		->where('id_ma_menu',$id_ma_menu)
+		->get();
+
+		return $result->result();
+	}
+}
